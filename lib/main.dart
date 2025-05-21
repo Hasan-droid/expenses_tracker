2bbaec3134f1082a2ac440b492863faeff748a02
Hasan Baydoun <hasan-net2010@hotmail.com>
2025-05-21 10:53:45 +0300

    124 to 127 theming

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,26 @@ import 'package:flutter/material.dart';
 
 import 'package:expenses_tracker/screen/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 34, 43, 204),
+);
 void main() {
-  runApp(const MaterialApp(home: Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimary,
+          foregroundColor: kColorScheme.primary,
+        ),
+        cardTheme: CardTheme().copyWith(color: kColorScheme.secondaryContainer),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.secondaryContainer,
+          ),
+        ),
+      ),
+      home: Expenses(),
+    ),
+  );
 }
