796b4ff33926e0b780463c12e5c23303d36c97e7
Hasan Baydoun <hasan-net2010@hotmail.com>
2025-05-23 21:20:53 +0300

    141. Understanding "Safe Areas"

--- lib/screen/expenses.dart
+++ lib/screen/expenses.dart
@@ -14,6 +14,7 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   void showNewItemOverLay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       constraints: BoxConstraints(maxHeight: double.infinity),
